5cbb16f5d6bdfd68744ad978470572c8feeb0a95
Kaider <kronoz@yandex.ru>
2025-09-15 22:01:29 -0700

    Initial Commit

---
+++ Focus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Focus
+//
+//  Created by Kaider on 15.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by Kaider on 15.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
