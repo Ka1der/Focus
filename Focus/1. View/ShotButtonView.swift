e99a1cc7766540834f8c06ace3e817fc0f0894d2
Kaider <kronoz@yandex.ru>
2025-09-15 22:22:51 -0700

    Добавлена кнопка снимка/записи

---
+++ Focus/1. View/ShotButtonView.swift
@@ -0,0 +1,40 @@
+//
+//  ShotButtonView.swift
+//  Focus
+//
+//  Created by Kaider on 15.09.2025.
+//
+
+import SwiftUI
+
+struct ShotButtonView: View {
+    
+    let firstCircleRadius: CGFloat = 55
+    let secondircleRadius: CGFloat = 50
+    let thirdircleRadius: CGFloat = 48
+    
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+            Button(action: action) {
+                ZStack {
+                    Text(title)
+                    Circle()
+                        .fill(Color.black)
+                        .frame(width: firstCircleRadius)
+                        .shadow(radius: 4)
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: secondircleRadius)
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: thirdircleRadius)
+                }
+        }
+    }
+}
+
+#Preview {
+    ShotButtonView(title: "", action: { print("tap") } )
+}
